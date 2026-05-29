e8792ecb9978f1e049de891ca05609fa31d8776e
SirSiri <chiossigustavo@gmail.com>
2026-05-29 00:07:44 -0300

    atualiza gerador, atualiza Dockerfile, adiciona mais comandos ao Makefile

--- controlador/gerador_ram_final/gerador.py
+++ controlador/gerador_ram_final/gerador.py
@@ -1,87 +1,73 @@
-# gerador de RAM (nao plota imagens, nao salva em csv, nao tem padrao wave_hard)
+from datetime import timedelta
+import os, gc, time, math, ctypes, random, psutil
 
-import os, time, math, random, psutil
-import matplotlib.pyplot as plt
-
-PADRAO = os.getenv("PADRAO", "pos_exp") 
-MAX_MB = int(os.getenv("MAX_MB", "512"))
-DURATION = float(os.getenv("DURATION", "180"))
+CICLO = float(os.getenv("CICLO", "60"))
+PADRAO = os.getenv("PADRAO", "pos_exp")
+MAX_MB = int(os.getenv("MAX_MB", "900"))
+DURACAO = float(os.getenv("DURACAO", "300"))
 SLEEP = float(os.getenv("SLEEP", "0.1"))
 INTERVALO = float(os.getenv("INTERVAL", "1.0"))
 K = 8
 
 process = psutil.Process(os.getpid())
 memory = None
-uso_ram = []
-timestamps = []
+libc = ctypes.CDLL("libc.so.6")
 
 def alloc(mb):
-    global memory 
-    memory = bytearray(int(mb * 1024 * 1024))    
-    time.sleep(0.5) 
+    global memory
+    if memory is not None:
+        del memory
 
-# tempo inicial
+    gc.collect()
+    libc.malloc_trim(0)
+    memory = bytearray(int(mb * 1024 * 1024))
+
 start_time = time.time()
-end_time = start_time + DURATION
+end_time = start_time + DURACAO
 current_time = start_time
 
 while current_time < end_time:
-    progresso = (current_time - start_time) / DURATION
-    
+
+    progresso = (current_time - start_time) / DURACAO
+    tempo_decorrido = current_time - start_time
+    fase = (tempo_decorrido % CICLO) / CICLO
+
     if PADRAO == "constant":
-        rss = process.memory_info().rss / (1024 * 1024)
-        uso_ram.append(rss)
-        timestamps.append(current_time - start_time)
+
+        alloc(MAX_MB)
         time.sleep(SLEEP)
         current_time = time.time()
         continue
-    
-    # calcular MB baseado no padrao
+
     if PADRAO == "ascending":
         mb = MAX_MB * progresso
-   
+
     elif PADRAO == "descending":
         mb = MAX_MB * (1 - progresso)
-   
+
     elif PADRAO == "wave":
         mb = MAX_MB * (0.5 + 0.5 * math.sin(4 * math.pi * progresso))
-    
+
     elif PADRAO == "pos_exp":
-        ciclos = 3
-        ciclo = (progresso * ciclos) % 1
-        # 80% do ciclo sobe exponencial
-        if ciclo < 0.8:
-            x = ciclo / 0.8
-            mb = MAX_MB * ((math.exp(K*x)-1) / (math.exp(K)-1))
-        # 20% cai reto
-        else:
-            x = (ciclo-0.8)/0.2
-            mb = MAX_MB*(1-x)
-    
+        mb = MAX_MB * ((math.exp(K * fase) - 1) / (math.exp(K) - 1))
+
     elif PADRAO == "neg_exp":
-        ciclos = 3
-        ciclo = (progresso * ciclos) % 1
-        if ciclo < 0.8:
-            x = ciclo / 0.8
-            mb = MAX_MB * (math.exp(-K * progresso) - math.exp(-K)) / (1 - math.exp(-K))
-        else:
-            x = (ciclo-0.8)/0.2
-            mb = MAX_MB * (1-x)
-            
+        mb = MAX_MB * ((math.exp(-K * fase) - math.exp(-K)) / (1 - math.exp(-K)))
+
     elif PADRAO == "partial_random":
-        cycle_num = int(progresso * (DURATION / 20))
+        cycle_num = int(progresso * (DURACAO / 20))
         mb = MAX_MB * 0.6 if cycle_num % 2 == 0 else random.uniform(0, MAX_MB)
-    
+
     elif PADRAO == "total_random":
         mb = random.uniform(0, MAX_MB)
-    
+
     else:
         raise ValueError("Parâmetro inválido")
-    
+
     alloc(mb)
     
-    # aguardar ate o proximo intervalo
     sleep_time = INTERVALO - (time.time() - current_time)
+
     if sleep_time > 0:
         time.sleep(sleep_time)
     current_time = time.time()
